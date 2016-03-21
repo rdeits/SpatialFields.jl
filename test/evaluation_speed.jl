79f9b4cb68d8e943bd9ab2e3c8a36a8ecc2ec492
Robin Deits <robin.deits@gmail.com>
2016-03-21 18:26:06 -0400

    cleanup

--- test/evaluation_speed.jl
+++ test/evaluation_speed.jl
@@ -1,17 +1,17 @@
 using SpatialFields
-using ProfileView
+# using ProfileView
 
 function evaluation_speed()
-    points = SpatialFields.Point{3, Float64}[rand(3) for i in 1:100]
-    normals = SpatialFields.Normal{3, Float64}[rand(3) for i in 1:100];
-    x = SpatialFields.Point{3, Float64}(0)
-    field = SpatialFields.HermiteRadialField(points, normals);
+    points = Point{3, Float64}[rand(3) for i in 1:100]
+    normals = Normal{3, Float64}[rand(3) for i in 1:100];
+    x = Point{3, Float64}(0)
+    field = HermiteRadialField(points, normals);
 
     println("Evaluation speed:")
-    Profile.clear()
-    for i = 1:1e4; SpatialFields.evaluate(field, x); end
-    @time @profile for i = 1:1e4; SpatialFields.evaluate(field, x); end
-    ProfileView.view()
+    # Profile.clear()
+    for i = 1:1e4; evaluate(field, x); end
+    @time for i = 1:1e4; evaluate(field, x); end
+    # ProfileView.view()
 end
 
 evaluation_speed()
