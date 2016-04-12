67c4525e386dcadef7e42c72920b7ea0aa7dbb12
Robin Deits <robin.deits@gmail.com>
2016-04-12 13:44:27 -0400

    fix iso level handling

--- src/meshing.jl
+++ src/meshing.jl
@@ -7,8 +7,8 @@ function surface_mesh(field::ScalarField{3}, bounds::HyperRectangle, iso_level=0
 
     lb = minimum(bounds) - w / 2.0
     window = HyperRectangle(Vec(lb), 2 * w)
-    sdf = SignedDistanceField(x -> evaluate(field, x), window, maximum(w) / sampling_rate)
-    mesh = HomogenousMesh(sdf, iso_level)
+    sdf = SignedDistanceField(x -> evaluate(field, x) - iso_level, window, maximum(w) / sampling_rate)
+    mesh = HomogenousMesh(sdf, 0.0)
 
     rescaled_points = Point{3,Float64}[Vec(v-1) ./ (Vec(size(sdf))-1) .* (2 * w) + lb for v in vertices(mesh)]
     return HomogenousMesh(rescaled_points, mesh.faces)
